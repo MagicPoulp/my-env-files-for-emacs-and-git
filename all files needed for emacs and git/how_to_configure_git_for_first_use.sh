90079f4cc972f150b959ee00ed2fbe0c98496bf8
Thierry Vilmart <t.vilmart@gmail.com>
2014-07-21 20:01:01 +0200

    small improvement fo env files

--- all files needed for emacs and git/how_to_configure_git_for_first_use.sh
+++ all files needed for emacs and git/how_to_configure_git_for_first_use.sh
@@ -5,4 +5,5 @@ git config --global remote.origin.push HEAD
 git config --global core.editor emacs
 git config --global user.name "Thierry Vilmart"
 git config --global user.email t.vilmart@gmail.com
+git config --global color.ui auto
 
