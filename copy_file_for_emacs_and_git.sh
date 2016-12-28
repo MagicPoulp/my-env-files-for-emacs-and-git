7d686fd2c856e3be2a305b5a4a19d7197406d1e7
Thierry Vilmart <thierry@fake.email.skynet.org>
2016-12-28 20:25:02 +0100

    I made git use the emacs_client script

--- copy_file_for_emacs_and_git.sh
+++ copy_file_for_emacs_and_git.sh
@@ -2,7 +2,7 @@
 
 git config --global push.default current
 git config --global remote.origin.push HEAD
-git config --global core.editor emacs
+git config --global core.editor emacs_client
 git config --global user.name "Thierry Vilmart"
 git config --global user.email "thierry@fake.email.skynet.org"
 git config --global color.ui auto
