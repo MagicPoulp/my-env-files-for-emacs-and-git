82f8f577fd8dca37075ab2e0581d17836d399aa5
Thierry Vilmart <thierry@fake.email.skynet.org>
2016-12-30 14:23:44 +0100

    cleanup the copying of files

--- copy_file_for_emacs_and_git.sh
+++ copy_file_for_emacs_and_git.sh
@@ -8,44 +8,45 @@ git config --global user.email "thierry@fake.email.skynet.org"
 git config --global color.ui auto
 
 DIR1=`dirname "$(readlink -f "$0")"`
-DIR_EMACS=$DIR1/emacs_and_git
-EXTRA_FOLDER=all_files_needed_for_emacs_and_git
+DIR_EMACS_FILES=$DIR1/emacs_and_git
 mkdir -p ~/bin
 
-cp $DIR_EMACS/$EXTRA_FOLDER/emacs_client ~/bin
-cp $DIR_EMACS/$EXTRA_FOLDER/.emacs ~/
-cp $DIR_EMACS/$EXTRA_FOLDER/.myvars ~/
-cp -r $DIR_EMACS/$EXTRA_FOLDER/.emacs.d ~/
-cp -r $DIR_EMACS/$EXTRA_FOLDER/.emacs_backup.d ~/
+cp $DIR_EMACS_FILES/emacs_client ~/bin
+cp $DIR_EMACS_FILES/.emacs ~/
+cp $DIR_EMACS_FILES/.myvars ~/
+cp -r $DIR_EMACS_FILES/.emacs.d ~/
+cp -r $DIR_EMACS_FILES/.emacs_backup.d ~/
 source ~/.myvars
 
 
-cp home_setup/user-dirs.dirs ~/.config/user-dirs.dirs
+DIR_HOME_SETUP=$DIR1/home_dir_setup
+cp $DIR_HOME_SETUP/user-dirs.dirs ~/.config/user-dirs.dirs
 
-mkdir ~/tmp
+mkdir -p ~/tmp
 
-mv ~/Desktop ~/desktop
-mv ~/Downloads ~/downloads
-mv ~/Documents ~/documents
+mv ~/Desktop ~/desktop 2> /dev/null
+mv ~/Downloads ~/downloads 2> /dev/null
+mv ~/Documents ~/documents 2> /dev/null
 
-mkdir ~/other
-mv ~/Music ~/other/
-mv ~/Pictures ~/other/
-mv ~/Music ~/other/
-mv ~/Pictures ~/other/
-mv ~/Public ~/other/
-mv ~/Templates ~/other/
-mv ~/Videos ~/other/
+mkdir -p ~/other
+mv ~/Music ~/other/ 2> /dev/null
+mv ~/Pictures ~/other/ 2> /dev/null
+mv ~/Music ~/other/ 2> /dev/null
+mv ~/Pictures ~/other/ 2> /dev/null
+mv ~/Public ~/other/ 2> /dev/null
+mv ~/Templates ~/other/ 2> /dev/null
+mv ~/Videos ~/other/ 2> /dev/null
 
 
 echo "--> all files have been copied"
 echo "a killall emacs may be needed"
 echo ""
-echo "--> still to do ONLY ONCE, as sudo:"
-echo ""
-echo "sudo cp -r ~/.emacs_backup.d/.emacs_working_for_root /root/.emacs"
+echo "--> TO DO ONCE PER INSTALLATION"
 echo ""
 echo 'sudo echo "source /home/thierry/.myvars" >> /root/.bashrc'
 echo 'echo "source /home/thierry/.myvars" >> ~/.bashrc'
 echo "sudo install emacs24 emacs-goodies-el"
-
+echo ""
+echo "--> FOR EVERY CHANGE TO ~/.emacs"
+echo ""
+echo "sudo cp -r ~/.emacs_backup.d/.emacs_working_for_root /root/.emacs"
