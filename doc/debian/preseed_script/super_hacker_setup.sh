992838bdac5d8fea3c29a8e428be769fff165539
Thierry Vilmart <thierry.vilmart@non.se.com>
2026-02-18 06:19:55 +0100

    feat: add isolcpu and irq pinning to the setup script
    This is useful for VMs

--- doc/debian/preseed_script/super_hacker_setup.sh
+++ doc/debian/preseed_script/super_hacker_setup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Thierry Vilmart
+#
+# Super hacker setup
+# sets up the firewall, isolcpus, interrupts pinning
 
 set -e
 
@@ -36,7 +40,7 @@ EOF
 # Apply the changes to the system database (This is the persistent way)
 dconf update
 
-echo "NumLock configured via system-wide database."
+echo "--> NumLock is configured via system-wide database."
 
 # --------------------------------------------------------
 # Setup firewall
@@ -44,8 +48,6 @@ echo "NumLock configured via system-wide database."
 # 1. Define the nftables configuration path
 NFT_CONF="/etc/nftables.conf"
 SYSCTL_CONF="/etc/sysctl.d/99-ipforward.conf"
-
-echo "Configuring hardened workstation firewall..."
 
 # 2. Write the nftables configuration
 cat <<EOF > "$NFT_CONF"
@@ -86,7 +88,6 @@ table inet filter {
 EOF
 
 # 3. Enable IP Forwarding in the Kernel (required for VM Internet)
-echo "Enabling IP Forwarding..."
 echo "net.ipv4.ip_forward=1" > "$SYSCTL_CONF"
 sysctl -p "$SYSCTL_CONF"
 
@@ -97,5 +98,82 @@ nft -f "$NFT_CONF"
 # 5. Ensure nftables starts on boot
 systemctl enable nftables
 
-echo "SUCCES: The firewall is configured for Desktop use and to allow a VM."
+echo "--> The firewall is configured for Desktop use and to allow a VM."
+
 # --------------------------------------------------------
+# Update the Kernel for isolcpus and for using VMs
+
+NEW_CONF="quiet isolcpus=4-19 nohz_full=4-19 rcu_nocbs=4-19 intel_iommu=on audit=0"
+sudo sed -i "s|^GRUB_CMDLINE_LINUX_DEFAULT=.*|GRUB_CMDLINE_LINUX_DEFAULT=\"$NEW_CONF\"|" /etc/default/grub
+echo "--> grub is configured to use isolcpus for using VMs."
+
+# --------------------------------------------------------
+# make a pinning file for interrups
+
+cat <<'EOF' > pin_irq.sh
+#!/bin/sh
+
+# see in /proc/interrupts
+USB_IRQ=$(grep xhci_hcd /proc/interrupts | awk '{print $1}' | tr -d ':' | tr '\n' ' ')
+
+# Target mask for CPU 5 (0x20 = 2^5)
+#TARGET_MASK="20"
+# Target mask for CPU 19
+TARGET_MASK="80000"
+
+# Assign i8042 (Keyboard) to CPU
+echo $TARGET_MASK | sudo tee /proc/irq/1/smp_affinity > /dev/null
+
+# Assign i8042 (Mouse/PS2) to CPU
+echo $TARGET_MASK | sudo tee /proc/irq/12/smp_affinity > /dev/null
+
+for NUMBER1 in $USB_IRQ; do
+    # Assign xhci_hcd (USB Host Controller - Primary) to CPU
+    echo $TARGET_MASK | sudo tee /proc/irq/$NUMBER1/smp_affinity > /dev/null
+done
+
+EXCLUDE="$USB_IRQ 1 12"
+
+# Define the CPU core range for the Host (CPUs 0, 1, 2, 3)
+HOST_CPU_MASK="F"
+
+echo ${HOST_CPU_MASK} > /proc/irq/default_smp_affinity
+
+echo "Starting IRQ affinity reset to Host Cores (0-3, mask $HOST_CPU_MASK)..."
+
+for irq_dir in /proc/irq/[0-9]*; do
+    IRQ_NUMBER=$(basename "$irq_dir")
+    IRQ_NUMBER=$(echo "$IRQ_NUMBER" | tr -d '[:space:]')
+    if ! echo " $EXCLUDE " | grep -q " $IRQ_NUMBER "; then
+        AFFINITY_FILE="$irq_dir/smp_affinity"
+        if [ -f "$AFFINITY_FILE" ]; then
+            CURRENT_AFFINITY=$(cat "$AFFINITY_FILE")
+            if [ "$CURRENT_AFFINITY" != "$HOST_CPU_MASK" ]; then
+                echo "$HOST_CPU_MASK" | sudo tee "$AFFINITY_FILE" > /dev/null
+                echo "   [SET] IRQ $IRQ_NUMBER: $CURRENT_AFFINITY -> $HOST_CPU_MASK"
+            fi
+        fi
+    else
+        echo "exclude $IRQ_NUMBER"
+    fi
+done
+
+echo "IRQ affinity reset complete."
+EOF
+
+chmod +x pin_irq.sh
+apt update
+apt-get install -y qemu-system libvirt-daemon-system libvirt-clients virtinst bridge-utils
+
+# install the pinning file for when the network changes and when a VM starts
+cp pin_irq.sh /etc/NetworkManager/dispatcher.d/99-irq-repin
+mkdir -p /etc/libvirt/hooks/qemu.d/win11/prepare/begin
+mkdir -p /etc/libvirt/hooks/qemu.d/win11/release/end
+cp irq-pin.sh /etc/libvirt/hooks/qemu.d/win11/release/begin
+
+echo "IRQ pinning is set in NetworkManager's dispatcher and in kvm"
+
+# --------------------------------------------------------
+
+echo "IMPORTANT: you must run: sudo update-grub"
+echo "IMPORTANT: you must run: sudo reboot"
